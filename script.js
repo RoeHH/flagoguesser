21eb0246a3cc2bdb625cf2b2eedd18f24d218e67
iccee0 <66622055+RoeHH@users.noreply.github.com>
2022-05-06 13:29:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function test(input){
-  console.log("fs")
+  const buttons = document.querySelectorAll("button");
+  for(const b of buttons){
+    b.addEventListener('click', event => {
+        console.log( event.target.id );
+   });
+  }
 }
