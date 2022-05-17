bdc975cc03f08025cf01ef4114aadd4e10ff334c
iccee0 <git@roeh.ch>
2022-05-17 17:16:18 +0200

    added high score

--- components/Flag.tsx
+++ components/Flag.tsx
@@ -1,10 +1,10 @@
 /** @jsx h */
 
-import { h } from "../client_deps.ts";
+import { h, tw } from "../client_deps.ts";
 
 export default function Flag({name: name}: {name: string}) {
   const baseURl = "https://countryflagsapi.com/svg/"
   return (
-    <img src={baseURl + name} />
+    <img class={tw`border-2 border-black`} src={baseURl + name} />
   );
 }
