752d987a39f34e59a51fa8a7f38a0e68681348b7
iccee0 <66622055+RoeHH@users.noreply.github.com>
2022-05-20 13:19:20 +0200

    /

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar(props: h.JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <nav class={tw`font-sans flex flex-row text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full`}>
      <div class={tw`mb-2 sm:mb-0 flex flex-row`}>
-        <img src="/favicon.ico" class={tw`h-[2rem]`} />
+        <img onclick="window.location='/'" src="/favicon.ico" class={tw`h-[2rem]`} />
         <a href="/" class={tw`text-2xl no-underline hover:text-indigo-800`}>Flago Guesser</a>
      </div>
      <div>
