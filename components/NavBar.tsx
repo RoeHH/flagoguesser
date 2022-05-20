f73447732d6d945aa029d36ce4b0cc84a56bc607
iccee0 <66622055+RoeHH@users.noreply.github.com>
2022-05-20 13:17:19 +0200

    flexiest

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,9 +4,9 @@ import { h, tw } from "../client_deps.ts";
 
 export default function NavBar(props: h.JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
-    <nav class={tw`font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full`}>
+    <nav class={tw`font-sans flex flex-row text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full`}>
      <div class={tw`mb-2 sm:mb-0 flex flex-col`}>
-        <img src="/favicon.ico" class={tw`h-10`} />
+        <img src="/favicon.ico" class={tw``} />
         <a href="/" class={tw`text-2xl no-underline hover:text-indigo-800`}>Flago Guesser</a>
      </div>
      <div>
